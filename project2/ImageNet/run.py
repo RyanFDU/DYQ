b2304f8485d442f43c59d342f58d4b9236003762
RyanFDU <ryanli.fdu@gmail.com>
2021-12-02 22:12:32 +0800

    everything fine

--- project2/ImageNet/run.py
+++ project2/ImageNet/run.py
@@ -37,7 +37,10 @@ def train(net_name, num_classes, image_path):
         ])
     }
 
-    train_dataset = datasets.CIFAR10(root=os.path.join(image_path, "train"), train=True, download=True, transform=data_transform["train"])
+    train_dataset = datasets.CIFAR10(root=os.path.join(image_path, "train"),
+                                     train=True,
+                                     download=True,
+                                     transform=data_transform["train"])
     train_num = len(train_dataset)
 
     # {'daisy':0, 'dandelion':1, 'roses':2, 'sunflower':3, 'tulips':4}
@@ -58,7 +61,10 @@ def train(net_name, num_classes, image_path):
                                                shuffle=True,
                                                num_workers=nw)
 
-    validate_dataset = datasets.CIFAR10(root=os.path.join(image_path, "val"), train=False, download=True, transform=data_transform["val"])
+    validate_dataset = datasets.CIFAR10(root=os.path.join(image_path, "val"),
+                                        train=False,
+                                        download=True,
+                                        transform=data_transform["val"])
     val_num = len(validate_dataset)
     validate_loader = torch.utils.data.DataLoader(validate_dataset,
                                                   batch_size=batch_size,
@@ -68,14 +74,15 @@ def train(net_name, num_classes, image_path):
     print("using {} images for training, {} images for validation.".format(
         train_num, val_num))
 
-    net = utils.get_network(net_name, use_gpu=False)
+    net = utils.get_network(net_name, use_gpu=True)
     # load pretrain weights
     model_weight_path = "./params/resnet34-pre.pth"
     if not os.access(model_weight_path, os.W_OK):
         synset_url = 'https://download.pytorch.org/models/resnet34-333f7ec4.pth'
         os.system('wget ' + synset_url)
+        os.system('mkdir ./params')
         os.system('mv ./resnet34-333f7ec4.pth ./params/resnet34-pre.pth')
-        
+
     # assert os.path.exists(model_weight_path), "file {} does not exist.".format(
     #     model_weight_path)
     net.load_state_dict(torch.load(model_weight_path, map_location=device))
@@ -174,16 +181,17 @@ def predict(netname, num_classes, img_path):
     model = utils.get_network(netname, num_classes, use_gpu=False)
     weights_path = "./params/resNet34.pth"
     model.load_state_dict(torch.load(weights_path, map_location=device))
-    
+
     # hacky way to deal with the upgraded batchnorm2D and avgpool layers...
     for i, (name, module) in enumerate(model._modules.items()):
         module = recursion_change_bn(model)
     model.avgpool = torch.nn.AvgPool2d(kernel_size=7, stride=1, padding=0)
-    
+
     model.eval()
 
     # hook the feature extractor
-    features_names = ['layer4','avgpool'] # this is the last conv layer of the resnet
+    features_names = ['layer4',
+                      'avgpool']  # this is the last conv layer of the resnet
     for name in features_names:
         model._modules.get(name).register_forward_hook(hook_feature)
 
@@ -195,7 +203,7 @@ def predict(netname, num_classes, img_path):
 
     params = list(model.parameters())
     weight_softmax = params[-2].data.numpy()
-    weight_softmax[weight_softmax<0] = 0
+    weight_softmax[weight_softmax < 0] = 0
 
     # logit = model.forward(img)
     # h_x = F.softmax(logit, 1).data.squeeze()
@@ -219,14 +227,16 @@ def predict(netname, num_classes, img_path):
 
     img = cv2.imread(img_path)
     height, width, _ = img.shape
-    heatmap = cv2.applyColorMap(cv2.resize(CAMs[0],(width, height)), cv2.COLORMAP_JET)
+    heatmap = cv2.applyColorMap(cv2.resize(CAMs[0], (width, height)),
+                                cv2.COLORMAP_JET)
     result = heatmap * 0.4 + img * 0.5
     cv2.imwrite('cam.jpg', result)
     result = result.astype(np.uint8)
-    cv2.imshow('',result)
+    cv2.imshow('', result)
     k = cv2.waitKey(0)
     if k == 27:
         cv2.destroyAllWindows()
+
 
 def recursion_change_bn(module):
     if isinstance(module, torch.nn.BatchNorm2d):
@@ -235,6 +245,7 @@ def recursion_change_bn(module):
         for i, (name, module1) in enumerate(module._modules.items()):
             module1 = recursion_change_bn(module1)
     return module
+
 
 def hook_feature(module, input, output):
     features_blobs.append(np.squeeze(output.data.cpu().numpy()))
@@ -246,13 +257,14 @@ def returnCAM(feature_conv, weight_softmax, class_idx):
     nc, h, w = feature_conv.shape
     output_cam = []
     for idx in class_idx:
-        cam = weight_softmax[class_idx].dot(feature_conv.reshape((nc, h*w)))
+        cam = weight_softmax[class_idx].dot(feature_conv.reshape((nc, h * w)))
         cam = cam.reshape(h, w)
         cam = cam - np.min(cam)
         cam_img = cam / np.max(cam)
         cam_img = np.uint8(255 * cam_img)
         output_cam.append(cv2.resize(cam_img, size_upsample))
     return output_cam
+
 
 if __name__ == '__main__':
     features_blobs = []
